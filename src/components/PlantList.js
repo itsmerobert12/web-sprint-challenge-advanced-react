5e7b189715185c5dd015ecdd1368ed4425a13bdd
Bobby Gomez <bobbygomez@Bobbys-MacBook-Pro.local>
2021-12-02 22:31:01 -0600

    fetching plant data

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -4,11 +4,21 @@ import axios from "axios";
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
   state = {
-    plants:[]
-  }
+    plants: []
+  };
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  componentDidMount() {
+    axios.get("http://localhost:3333/plants")
+      .then(response => {
+        this.setState({
+          plants: response.data
+        });
+      })
+      .catch(err => console.error(err));
+  };
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
@@ -39,3 +49,4 @@ export default class PlantList extends Component {
     );
   }
 }
+
